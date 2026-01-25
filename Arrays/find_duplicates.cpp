ffaaf79bbec830eda8b7aa37ea8746ea1bc64815
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-25 23:44:42 +0530

    find duplicates

---
+++ Arrays/find_duplicates.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int ans=0;
+int find_dupl(int arr[], int n) {
+    for(int i=0;i<n;i++) {
+        ans=ans^arr[i];
+    }
+    for(int i=1;i<n;i++) {
+        ans=ans^i;
+    }
+    return ans;
+}
+
+
+
+
+int main() {
+    int arr[]={1,1,2,3,4};
+    int dupl= find_dupl(arr, 5);
+    cout<<"find duplicates are: "<<dupl<<endl;
+}
+
