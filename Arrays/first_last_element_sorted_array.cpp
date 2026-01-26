8cccc5a59d7cb56c16164e0ffdd3423e54e717f7
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-26 17:04:48 +0530

    first_last_element_sorted_array

---
+++ Arrays/first_last_element_sorted_array.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+int first_occ(int arr[], int n, int key) {
+    int s=0;
+    int e=n-1;
+    int ans=-1;
+    while(s<=e) {
+        int mid=s+(e-s)/2;
+        if (arr[mid]==key) {
+            ans=mid;
+            e=mid-1;
+        }
+        else if(arr[mid]> key)  {
+            e=mid-1;
+        }
+        else if(key>arr[mid]) {
+            s=mid+1;
+        }
+
+    }
+    return ans;
+}
+
+
+int last_occ(int arr[], int n, int key) {
+    int s=0;
+    int e=n-1;
+    int ans=-1;
+    while(s<=e) {
+        int mid=s+(e-s)/2;
+        if (arr[mid]==key) {
+            ans=mid;
+            s=mid+1;
+        }
+        else if(arr[mid]>key) {
+            e=mid-1;
+        }
+        else if(key>arr[mid]){
+            s=mid+1;
+        }
+    }
+    return ans;
+
+}
+
+
+
+
+
+
+
+
+int main() {
+
+    int arr[]={1,2,2,2,3,3,4};
+    cout<<"the first occurance in the sorted array  of the element is :"<<first_occ(arr, 7, 2)<<endl;
+    cout<<"the last occurance in the sorted array  of the element is :"<<last_occ(arr, 7, 2);
+}
