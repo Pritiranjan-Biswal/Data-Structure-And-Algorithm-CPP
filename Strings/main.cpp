596212615ee426378683ad259940c5d1dff38524
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-01-30 22:09:29 +0530

    palindroime

--- Strings/main.cpp
+++ Strings/main.cpp
@@ -1,18 +1,65 @@
+// #include<iostream>
+// using namespace std;
+
+// int getLength(char name[]) {
+//     int count=0;
+//     for(int i=0; name[i] != '\0'; i++) {
+//         count++;
+//     }
+//     return count;
+// }
+
+
+
+// int main() {
+//     char ch[]="kunabiswal";
+//     cout<<getLength(ch);
+//     return 0;
+// }
+
+
+
+//kuna => anuk
+
+
+
 #include<iostream>
 using namespace std;
 
-int getLength(char name[]) {
-    int count=0;
-    for(int i=0; name[i] != '\0'; i++) {
-        count++;
+
+
+// void rev_str(char name[], int n) {
+//     int s=0;
+//     int e =n-1;
+//     while(s<e) {
+//         swap(name[s], name[e]);
+//         s++;
+//         e--;
+ 
+//        }
+//   }
+
+bool palindrome(char a[], int n) {
+    int s=0;
+    int e=n-1;
+    while(s<e) {
+        if(a[s] != a[e]) {
+            return 0;
+        }
+        else {
+            s++;
+            e--;
+        }
+        return 1;
     }
-    return count;
+    
 }
 
 
 
+
 int main() {
-    char ch[]="kunabiswal";
-    cout<<getLength(ch);
-    return 0;
+    char ch[]="kuuk";
+    
+    cout<<palindrome(ch, 4);
 }
