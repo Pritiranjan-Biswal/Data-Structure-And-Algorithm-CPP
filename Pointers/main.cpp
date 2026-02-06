1a130b636e263c1a9473b3b36abe13a8a7c5d843
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-02-06 22:59:36 +0530

    njm

--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -18,16 +18,23 @@ int main() {
     
     // cout<<p;
 
-    int i=5;
-    int *q=&i;
-    cout<<q<<endl;
-    cout<<*q<<endl;
+    // int i=5;
+    // int *q=&i;
+    // cout<<q<<endl;
+    // cout<<*q<<endl;
 
-    int *p=0;
-    p=&i;
+    // int *p=0;
+    // p=&i;
 
-    cout<<p<<endl;
-    cout<<*p<<endl;
+    // cout<<p<<endl;
+    // cout<<*p<<endl;
 
+
+    int num=5;
+    int a=num;
+
+    cout<<"a before "<<num<<endl;
+    a++;
+    cout<<" a after "<<num<<endl;
 
 }
