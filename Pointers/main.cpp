0b26cdbf3372a048dae77065bfe9b93036d6f749
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-02-06 22:50:12 +0530

    pouinters

--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -2,13 +2,20 @@
 using namespace std;
 
 int main() {
+    // int x = 10;
+    // int *ptr = &x;
+
+    // cout << "Value of x: " << x << endl;
+    // cout << "Address of x: " << &x << endl;
+    // cout << "Pointer ptr stores: " << ptr << endl;
+    // cout << "Value at ptr (*ptr): " << *ptr << endl;
+
     int x = 10;
-    int *ptr = &x;
+    int *p = &x;
+    cout<< x <<endl;
+    cout << *p << endl;
+    cout << &x << endl;
+    
+    cout<<p;
 
-    cout << "Value of x: " << x << endl;
-    cout << "Address of x: " << &x << endl;
-    cout << "Pointer ptr stores: " << ptr << endl;
-    cout << "Value at ptr (*ptr): " << *ptr << endl;
-
-    return 0;
 }
