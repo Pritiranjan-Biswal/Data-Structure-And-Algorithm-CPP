3cba42b977825ccb7969db6d4082300ebe5260a7
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-02-06 10:55:04 +0530

    push zeros to end

---
+++ Practice/move_zero_to_end.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void  move_zero(int arr[], int n) {
+    int non_zero=0;
+    for(int j=0; j<n; j++) {
+        if(arr[j]!=0) {
+            swap(arr[j], arr[non_zero]);
+            non_zero++;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+
+    int arr[]={1,0,2,3,0,2,9,0,0};
+
+    move_zero(arr, 9);
+    for(int i=0; i<9; i++) {
+        cout<<arr[i]<<" ";
+    }
+
+
+}
+
+
