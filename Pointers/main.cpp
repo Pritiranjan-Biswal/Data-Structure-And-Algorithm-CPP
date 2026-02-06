d74126b62395925b358497391e4be41a3bbf18af
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-02-06 23:31:09 +0530

    final

--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -30,25 +30,32 @@ int main() {
     // cout<<*p<<endl;
 
 
-    int num=5;
-    int a=num;
+    // int num=5;
+    // int a=num;
 
-    cout<<"a before "<<num<<endl;
-    a++;
-    cout<<" a after "<<num<<endl;
+    // cout<<"a before "<<num<<endl;
+    // a++;
+    // cout<<" a after "<<num<<endl;
 
-    int *p=&num;
-    cout<<"before "<< num<<endl;
-    (*p)++;
-    cout<<"after "<<num<<endl; 
-
-
-    int *q=p;
-    cout<<p<<"-"<<q<<endl;
-    cout<<*p<<"-"<<*q<<endl;
+    // int *p=&num;
+    // cout<<"before "<< num<<endl;
+    // (*p)++;
+    // cout<<"after "<<num<<endl; 
 
 
+    // int *q=p;
+    // cout<<p<<"-"<<q<<endl;
+    // cout<<*p<<"-"<<*q<<endl;
 
+
+
+    int i=3;
+    int *t= &i;
+    *t=*t+1;
+    cout<<*t<<endl;
+    cout<<"before t"<<t<<endl;
+    t=t+1;
+    cout<<"after t"<<t<<endl;
 
 
 
