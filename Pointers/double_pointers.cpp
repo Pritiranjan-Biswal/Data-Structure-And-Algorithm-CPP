c08fba160e05cafdca23a7def120b1dd11df90cd
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-02-10 21:44:51 +0530

    q1

--- Pointers/double_pointers.cpp
+++ Pointers/double_pointers.cpp
@@ -39,8 +39,49 @@ int main() {
 
 
 
-    int *p=0;
-    int first=110;
-    *p=first;
-    cout<<*p<<endl;
-}
+    // int *p=0;
+    // int first=110;
+    // *p=first;
+    // cout<<*p<<endl;
+
+    // int first=8;
+    // int second=11;
+    // int *third=&second;
+    // first=*third;
+    // *third=*third+2;
+    // cout<<first<<" "<<second<<endl;
+
+
+    float f=12.5;
+    float p=21.5;
+    float *ptr=&f;
+    (*ptr)++;
+    *ptr=p;
+    cout<<*ptr<< " "<<f<<" "<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} 
