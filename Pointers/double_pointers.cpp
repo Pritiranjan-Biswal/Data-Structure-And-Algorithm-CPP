f4bead93505133c0414ded5c2001143e6c7eccb6
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-02-10 20:09:13 +0530

    q1

--- Pointers/double_pointers.cpp
+++ Pointers/double_pointers.cpp
@@ -5,16 +5,25 @@ using namespace std;
 
 
 int main() {
-    int i=5;
-    int *ptr=&i;
-    int **ptr2=&ptr;
+    // int i=5;
+    // int *ptr=&i;
+    // int **ptr2=&ptr;
 
-    cout<<i<<endl;
-    cout<<ptr<<endl;
-    cout<<*ptr<<endl;
-    cout<< &ptr<<endl;
-    cout<<*ptr2<<endl;
-    cout<<&ptr2<<endl;
+    // cout<<&i<<endl;
+    // cout<<ptr<<endl;
+    // cout<<*ptr2<<endl;
+    // cout<< &ptr<<endl;
+    // cout<<*ptr2<<endl;
+    // cout<<&ptr2<<endl;
+
+    int first=8;
+    int second=18;
+
+    int *ptr=&second;
+    *ptr=9;
+
+    cout<<first<<" "<<second<<endl;
+
 
 
 }
