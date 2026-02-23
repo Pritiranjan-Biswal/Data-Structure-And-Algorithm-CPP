7175eab54758ffb95beb4a23dbd830886ec5745c
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-02-23 19:54:23 +0530

    largest_tuf

---
+++ Take You Forward/largest_element.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void largestele(int arr[], int n) {
+    int largest=arr[0];
+    for(int i=0; i<n; i++) {
+        if(arr[i]> largest) {
+            largest=arr[i];
+        }
+    }
+    cout<<largest;
+}
+
+
+
+
+
+
+
+
+
+int main() {
+    int arr[]= {1,2,4,2,1};
+    largestele(arr, 5);
+}
