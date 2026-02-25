e71e050f639e2ad05876207b156b132767164d30
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-02-25 09:41:54 +0530

    nwk

--- GFG/prefix_sum.cpp
+++ GFG/prefix_sum.cpp
@@ -14,19 +14,19 @@ using namespace std;
 //     }
 // }
 
-void prefix_sum(int arr[], int n) {
-    int sum=0;
-    for(int i=0; i<n; i++) {
-        sum=sum+arr[i];
-        arr[i]=sum;
-    }
-    for(int i=0; i<n; i++) {
-        cout<<arr[i]<<" ";
+    void prefix_sum(int arr[], int n) {
+        int sum=0;
+        for(int i=0; i<n; i++) {
+            sum=sum+arr[i];
+            arr[i]=sum;
+        }
+        for(int i=0; i<n; i++) {
+            cout<<arr[i]<<" ";
+        }
+
     }
 
-}
-
-int main() {
-    int arr[]={1,2,3,4,5};
-    prefix_sum(arr,5);
-}
+    int main() {
+        int arr[]={1,2,3,4,5};
+        prefix_sum(arr,5);
+    }
