3a9dc40d418e77a09cb1513a9b183b1af23e6f71
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-05-04 22:29:09 +0530

    pointer revision

--- Arraya STL/main.cpp
+++ Arraya STL/main.cpp
@@ -2,13 +2,15 @@
 using namespace std;
 
 int main() {
-    int num=5;
-    int a=num;
-    cout<<"a before "<<num<<endl;
-    a++;
-    cout<<"a after "<<a<<endl;
-    int *p=&num;
-    cout<<"before "<<num<<endl;
-    (*p)++;
-    cout<<"after "<<num<<endl;
+    // int num=5;
+    // int a=num;
+    // cout<<"a before "<<num<<endl;
+    // a++;
+    // cout<<"a after "<<num<<endl;
+    // int *p=&num;
+    // cout<<"before "<<num<<endl;
+    // (*p)++;
+    // cout<<"after "<<num<<endl;
+    int arr[5]={1,2,3,4,5};
+    cout<<arr;
 }
