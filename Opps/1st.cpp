f7769efff09b08169d550bcba05348fa5dee7334
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-05-16 02:26:26 +0530

    oops

---
+++ Opps/1st.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Student{
+    public:
+    string name;
+    int age, roll_number;
+    string grade;
+};
+
+int main() {
+    Student s1;
+    s1.name="kuna";
+    s1.age=21;
+    s1.roll_number=342;
+    s1.grade="A";
+    cout<<s1.name;
+}
+
+
+
+
+
+
+
+
+
+
